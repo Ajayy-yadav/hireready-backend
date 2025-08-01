3ab0c510347f9a647c55b3b91b12d1e6c5c66d9f
Ajayy-yadav <ajaychintu2003@gmail.com>
2025-08-02 01:13:27 +0530

    updated for swagger

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,12 +2,27 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiBody } from '@nestjs/swagger';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post("create")
+  @ApiBody({
+    type: CreateUserDto,
+    description: 'Create a new user',
+    examples: {
+      example1: {
+        summary: 'Sample user',
+        value: {
+          username: 'john_doe',
+          email: 'john@example.com',
+          currentRole: 'Developer',
+        },
+      }
+    },
+  })
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
@@ -19,6 +34,22 @@ export class UserController {
   }
 
   @Patch("update/:id")
+  @ApiBody({
+    type: UpdateUserDto,
+    description: 'Update user details',
+    examples: {
+      example1: {
+        summary: 'Sample user',
+        value: {
+          username: 'john_doe',
+          email: 'john@example.com',
+          currentRole: 'Developer',
+          resumeUrl: 'https://example.com/resume.pdf',
+          imageUrl: 'https://example.com/image.jpg',
+        },
+      }
+    },
+  })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(id, updateUserDto);
   }
