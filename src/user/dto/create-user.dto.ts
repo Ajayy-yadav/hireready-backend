b822ae5c83fff3f0d69dab63491386eebde7eb1f
Ajayy-yadav <ajaychintu2003@gmail.com>
2025-08-16 18:02:30 +0530

    added validation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,15 @@
+import { IsString, IsEmail, IsUUID } from 'class-validator';
+
 export class CreateUserDto {
+    @IsUUID()
     id: string;
+    
+    @IsString()
     username: string;
+    
+    @IsEmail()
     email: string;
+    
+    @IsString()
     currentRole: string;
 }
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,7 +1,13 @@
 import { PartialType } from '@nestjs/swagger';
+import { IsString, IsOptional } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @IsOptional()
+    @IsString()
     resumeKey?: string;  // Store S3 key for resume
+    
+    @IsOptional()
+    @IsString()
     imageKey?: string;   // Store S3 key for profile image
 }
